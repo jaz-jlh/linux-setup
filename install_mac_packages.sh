1e4e1ee27e32267e1b7896edffb1cbf003538007
Jaz <jaz@Jazs-MacBook-Pro.local>
2024-12-09 17:10:05 -0500

    initial version of mac setup

---
+++ install_mac_packages.sh
@@ -0,0 +1,9 @@
+brew install $(awk '{print $1'} mac_packages.txt)
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# p10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# auto suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
